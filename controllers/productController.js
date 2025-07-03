f7c9f57df7b4d30cd9f9a692fe0f9f3bcfd24355
Sarth-dev <sarthakkale2003@gmail.com>
2025-07-03 23:28:11 +0530

    edit

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,9 +11,31 @@ const createProduct = async (req, res) => {
   res.status(201).json(newProduct);
 };
 
+const editProducts = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, category } = req.body;
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { name, price, category },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    console.error('Error updating product:', err);
+    res.status(500).json({ message: 'Server error while updating product' });
+  }
+};
+
 const deleteProduct = async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.status(204).end();
 };
 
-module.exports = {getProducts , createProduct ,deleteProduct}
+module.exports = {getProducts , createProduct, editProducts ,deleteProduct}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,14 @@ const {
   getProducts,
   createProduct,
   deleteProduct,
+  editProducts,
 } = require('../controllers/productController');
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.post('/', createProduct);
+router.put('/:id',editProducts);
 router.delete('/:id', deleteProduct);
 
 module.exports= router;
